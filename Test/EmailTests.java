42cd66204475d1fd3a524793b278d55a7bc363d1
ryanlacey <119774249@umail.ucc.ie>
2021-12-04 19:34:21 +0000

    password authentication tests created

--- Test/EmailTests.java
+++ Test/EmailTests.java
@@ -4,24 +4,24 @@ import org.junit.jupiter.api.Test;
 public class EmailTests {
 
     @Test
-    public void emailAuthenticatorCorrect(){
+    public void emailAuthenticatorCorrectTester(){
         Assertions.assertTrue(Controller.emailAuthenticator("ryan@mail.com") == true);
 
     }
 
     @Test
-    public void emailAuthenticatorNoAtSymbol(){
+    public void emailAuthenticatorNoAtSymbolTester(){
         Assertions.assertTrue(Controller.emailAuthenticator("ryanmail.com") == false);
     }
 
     @Test
-    public void emailAuthenticatorSpaces(){
+    public void emailAuthenticatorSpacesTester(){
         Assertions.assertTrue(Controller.emailAuthenticator("ryan @mail.com") == false);
     }
 
     @Test
-    public void emailAuthenticatorFullStop(){
-        Assertions.assertTrue(Controller.emailAuthenticator("ryan@mailcom"));
+    public void emailAuthenticatorNoFullStopTester(){
+        Assertions.assertTrue(Controller.emailAuthenticator("ryan@mailcom") == false);
     }
 
 }
--- Test/PasswordTests.java
+++ Test/PasswordTests.java
@@ -1,2 +1,30 @@
-public class PasswordTests {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PasswordAuthenticatorTests {
+
+    @Test
+    public void correctTester(){
+        Assertions.assertTrue(Controller.passwordAuthenticator("greencat1!") == true);
+    }
+
+    @Test
+    public void lengthTester(){
+        Assertions.assertTrue(Controller.passwordAuthenticator("gren1!") == false);
+    }
+
+    @Test
+    public void blankSpaceTester(){
+        Assertions.assertTrue(Controller.passwordAuthenticator("greencat 1!") == false);
+    }
+
+    @Test
+    public void digitTester(){
+        Assertions.assertTrue(Controller.passwordAuthenticator("greencat!") == false);
+    }
+
+    @Test
+    public void symbolTester(){
+        Assertions.assertTrue(Controller.passwordAuthenticator("greencat1") == false);
+    }
 }
