0cd63ffd99e486e941f62d4db3a161155da46ef8
ryanlacey <119774249@umail.ucc.ie>
2021-12-04 23:12:08 +0000

    Basic UI created, still missing a password verification to ensure password was not entered incorrectly by accident

--- src/View.java
+++ src/View.java
@@ -34,24 +34,50 @@ public class View {
         TextField emailTextField = new TextField();
         grid.add(emailTextField, 1, 1);
 
-        Button submitButton = new Button("Register");
-        grid.add(submitButton, 1, 2);
+        Label passwordLabel = new Label("Password");
+        grid.add(passwordLabel, 0, 2);
 
-        final Text toShow = new Text();
-        grid.add(toShow, 1, 3);
+        TextField passwordTextField = new TextField();
+        grid.add(passwordTextField, 1, 2);
+
+        Button submitButton = new Button("Register");
+        grid.add(submitButton, 1, 3);
+
+        final Text successPrompt = new Text("Registration Successful!");
+        final Text emailFail = new Text("Invalid email, please try again");
+        final Text passwordFail = new Text("""
+                            Invalid password, must be:
+                            minimum 7 characters long,
+                            contain at least 1 letter,
+                            contain at least 1 number
+                            and one of the following symbols '^&@!'""");
 
         submitButton.setOnAction(new EventHandler<ActionEvent>() {
-
             @Override
             public void handle(ActionEvent e) {
-                Controller.emailAuthenticator(String.valueOf(emailTextField));
+
+                if (grid.getChildren().contains(emailFail)){
+                    grid.getChildren().remove(emailFail);
+                }
+                else if(grid.getChildren().contains(passwordFail)) {
+                    grid.getChildren().remove(passwordFail);
+                }
+                else if(grid.getChildren().contains(successPrompt)){
+                    grid.getChildren().remove(successPrompt);
+                }
+                if (!Controller.emailAuthenticator(String.valueOf(emailTextField.getText()))) {
+                    grid.add(emailFail, 1, 4);
+                }
+                else if (!Controller.passwordAuthenticator(String.valueOf(passwordTextField.getText()))) {
+                    grid.add(passwordFail, 1, 4);
+                }
+                else{grid.getChildren();
+                    grid.add(successPrompt,1,4);
             }
-        });
-        stage.show();
-    }
+        }
+    });
+                stage.show();
+}}
 
-
-
-}
 
 
