a732bf9443be1288612df7a4cfa30af3c7209db1
ryanlacey <119774249@umail.ucc.ie>
2021-12-04 23:12:08 +0000

    Basic UI created, still missing a password verification to ensure password was not entered incorrectly by accident

--- src/View.java
+++ src/View.java
@@ -11,8 +11,6 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.util.Objects;
-
 public class View {
 
 
@@ -24,11 +22,11 @@ public class View {
         grid.setVgap(5);
         grid.setPadding(new Insets(20, 20, 20, 20));
 
-        Scene registrationScene = new Scene(grid, 600, 400);
-        stage.setScene(registrationScene);
+        Scene scene = new Scene(grid, 300, 275);
+        stage.setScene(scene);
 
         Text title = new Text("Sign Up Below");
-        grid.add(title, 0, 0);
+        grid.add(title, 0, 0, 2, 1);
 
         Label emailLabel = new Label("E-mail");
         grid.add(emailLabel, 0 ,1);
@@ -42,14 +40,8 @@ public class View {
         TextField passwordTextField = new TextField();
         grid.add(passwordTextField, 1, 2);
 
-        Label passwordConfirmationLabel = new Label("Re-Enter Password");
-        grid.add(passwordConfirmationLabel, 0, 3);
-
-        TextField passwordConfirmationTextField = new TextField();
-        grid.add(passwordConfirmationTextField, 1, 3);
-
         Button submitButton = new Button("Register");
-        grid.add(submitButton, 1, 4);
+        grid.add(submitButton, 1, 3);
 
         final Text successPrompt = new Text("Registration Successful!");
         final Text emailFail = new Text("Invalid email, please try again");
@@ -59,33 +51,31 @@ public class View {
                             contain at least 1 letter,
                             contain at least 1 number
                             and one of the following symbols '^&@!'""");
-        final Text passwordMatchError = new Text("Error, passwords do not match");
 
-        submitButton.setOnAction(new EventHandler<>() {
+        submitButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
-                if (grid.getChildren().contains(emailFail)) {
+
+                if (grid.getChildren().contains(emailFail)){
                     grid.getChildren().remove(emailFail);
-                } else if (grid.getChildren().contains(passwordFail)) {
+                }
+                else if(grid.getChildren().contains(passwordFail)) {
                     grid.getChildren().remove(passwordFail);
-                } else if (grid.getChildren().contains(passwordMatchError)) {
-                    grid.getChildren().remove(passwordMatchError);
-                } else if (grid.getChildren().contains(successPrompt)) {
+                }
+                else if(grid.getChildren().contains(successPrompt)){
                     grid.getChildren().remove(successPrompt);
                 }
-                if (!Controller.emailAuthenticator(emailTextField.getText())) {
-                    grid.add(emailFail, 1, 5);
-                } else if (!Objects.equals(passwordTextField.getText(), passwordConfirmationTextField.getText())) {
-                    grid.add(passwordMatchError, 1, 5);
-                } else if (!Controller.passwordAuthenticator(passwordTextField.getText())) {
-                    grid.add(passwordFail, 1, 5);
-                } else {
-                    grid.getChildren();
-                    grid.add(successPrompt, 1, 5);
-
+                if (!Controller.emailAuthenticator(String.valueOf(emailTextField.getText()))) {
+                    grid.add(emailFail, 1, 4);
                 }
+                else if (!Controller.passwordAuthenticator(String.valueOf(passwordTextField.getText()))) {
+                    grid.add(passwordFail, 1, 4);
+                }
+                else{grid.getChildren();
+                    grid.add(successPrompt,1,4);
             }
-        });
+        }
+    });
                 stage.show();
 }}
 
